2f3ae8df359483ec72f8ebdd90f7c0b8b34480ae
Eren Taşkıran <101654590+erentaskiran@users.noreply.github.com>
2025-01-30 13:30:33 +0300

    Create login page using shadcn  (#11)
    * setup main page and create header component
    
    * add prettier
    
    * change npm ci to npm install
    
    * change npm ci to npm install
    
    * format code
    
    * update README.md
    
    * add docs
    
    * format code
    
    * setup login page
    
    * fix format and build error
    
    ---------
    
    Co-authored-by: Eren Taşkıran <erentaskiran@Erens-MacBook-Pro.local>
    Co-authored-by: Eren Taşkıran <erentaskiran@Mac.home>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,18 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import './globals.css'
-import { Header } from '@/components/header'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Community-Portal',
-  description: 'Best community portal for your community',
+  title: 'Community Portal',
+  description: 'Community Portal for everyone',
 }
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <div className="flex flex-col">{children}</div>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
