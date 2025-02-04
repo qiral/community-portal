ab502c42711b83b2711e2e0243496c7405eef55f
Eren Taskiran <eren.tas3535@gmail.com>
2025-02-04 21:57:55 +0300

    add avatar to header

--- apps/main/src/components/header.tsx
+++ apps/main/src/components/header.tsx
@@ -1,7 +1,17 @@
 import Link from 'next/link'
 import { Button } from '@community/ui'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 
 export function Header() {
+  const isLoggedIn = true
   return (
     <header className="border-b px-8">
       <div className="px-4n container flex h-16 min-w-[100%] items-center justify-between">
@@ -17,16 +27,34 @@ export function Header() {
             <Link href="/feed">Feed</Link>
           </nav>
         </div>
-        <div className="flex items-center gap-4">
-          <Link href="/login">
-            {' '}
-            <Button variant="outline">Giriş Yap</Button>{' '}
-          </Link>
-          <Link href="/register">
-            {' '}
-            <Button variant="outline">Kaydol</Button>{' '}
-          </Link>
-        </div>
+        {!isLoggedIn ? (
+          <div className="flex items-center gap-4">
+            <Link href="/login">
+              {' '}
+              <Button variant="outline">Giriş Yap</Button>{' '}
+            </Link>
+            <Link href="/register">
+              {' '}
+              <Button variant="outline">Kaydol</Button>{' '}
+            </Link>
+          </div>
+        ) : (
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>İsim Soyisim</DropdownMenuLabel>
+              <DropdownMenuItem>Hesabım</DropdownMenuItem>
+              <DropdownMenuItem>Ayarlar</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Çıkış yap</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
       </div>
     </header>
   )
