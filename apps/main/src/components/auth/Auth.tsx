10bb0a3c6ca6f841ef7c86752551ccaff7a84c29
Ahmet Arda ORAK <ahmetardaorak@gmail.com>
2025-02-21 20:23:29 +0300

    reverted stupidness

--- apps/main/src/components/auth/Auth.tsx
+++ apps/main/src/components/auth/Auth.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use server'
 
 import { supabase } from '@/lib/supabaseClient'
 
--- apps/main/src/lib/supabaseClient.ts
+++ apps/main/src/lib/supabaseClient.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://dbfxippjivuqpdzlkenx.supabase.co'
-const SUPABASE_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRiZnhpcHBqaXZ1cXBkemxrZW54Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczODQ3ODU0MiwiZXhwIjoyMDU0MDU0NTQyfQ.qHHyD7WFQCFe0IdBCI9Y7MSOleFdt9PGbGkywm6V2p4'
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY!
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
