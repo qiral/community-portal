673a18fd995f3bf40d229b3045d9f7aa44d572b5
Ahmet Bilal Yazıcıoğlu <ahmetbilalyazicioglu@gmail.com>
2025-03-20 06:43:45 +0300

    Admin edit page added. (#39)
    * Admin edit panel added
    
    * Admin edit page added

--- packages/config/eslint-config-custom/react.js
+++ packages/config/eslint-config-custom/react.js
@@ -1,17 +1,17 @@
 module.exports = {
   extends: [
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "./index.js",
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    './index.js',
   ],
   rules: {
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
-}; 
+}
