438808cb7f9a4cbb84d4426befd78e58092442d1
metharda <ahmetardaorak@gmail.com>
2025-03-22 15:02:53 +0300

    moreeeeeee error fix

--- apps/main/src/lib/useSelectedClub.ts
+++ apps/main/src/lib/useSelectedClub.ts
@@ -1,23 +1,40 @@
-import { create, SetState } from 'zustand'
+import { create } from 'zustand'
 import { icons } from '@/components/admin-page/acc-switch'
 
 interface ClubState {
   selectedClub: { id: string; name: string; type: string; logoId: keyof typeof icons } | null
-  setSelectedClub: (club: { id: string; name: string; type: string }) => void
+  setSelectedClub: (club: {
+    id: string
+    name: string
+    type: string
+    logoId: keyof typeof icons
+  }) => void
 }
 
-export const useSelectedClub = create<ClubState>((set: SetState<ClubState>) => {
-  let initialSelectedClub: { id: string; name: string; type: string } | null = null
+export const useSelectedClub = create<ClubState>((set) => {
+  let initialSelectedClub: {
+    id: string
+    name: string
+    type: string
+    logoId: keyof typeof icons
+  } | null = null
 
   if (typeof window !== 'undefined') {
     const storedClub = localStorage.getItem('selectedClub')
-    initialSelectedClub = storedClub ? JSON.parse(storedClub) : null
+    if (storedClub) {
+      initialSelectedClub = JSON.parse(storedClub)
+    }
   }
 
   return {
     selectedClub: initialSelectedClub,
-    setSelectedClub: (club: { id: string; name: string; type: string }) => {
-      set({ selectedClub: club })
+    setSelectedClub: (club: {
+      id: string
+      name: string
+      type: string
+      logoId: keyof typeof icons
+    }) => {
+      set((state) => ({ ...state, selectedClub: club }))
 
       if (typeof window !== 'undefined') {
         localStorage.setItem('selectedClub', JSON.stringify(club))
